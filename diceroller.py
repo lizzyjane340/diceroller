14110de955a664d61ce7399cae9266875a50ee5c
Lizzy Jane Ferris <lizzyjane0121@protonmail.com>
2021-07-16 16:32:55 +0100

    Add basic CLI error handling

--- diceroller.py
+++ diceroller.py
@@ -4,4 +4,10 @@ import sys
 def dice(n):
     print(f"You rolled a {random.randint(1, n)}")
 
+if len(sys.argv) != 2:
+    print("Only takes one argument!")
+    sys.exit(1)
+
 dice(int(sys.argv[1]))
+
+
